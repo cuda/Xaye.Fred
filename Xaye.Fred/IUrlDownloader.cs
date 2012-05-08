988ab0c2898f9b46abe9f5f161051bd5988df193
Marcus Cuda <marcus@cuda.org>
2012-05-08 11:09:19 -0400

    updated downloader so a Fred object can be called by multiple threads

--- Xaye.Fred/IUrlDownloader.cs
+++ Xaye.Fred/IUrlDownloader.cs
@@ -12,18 +12,16 @@ namespace Xaye.Fred
     // needed so we could mock web requests. 
     internal class WebClientDownloader : IUrlDownloader
     {
-        private readonly WebClient _webClient = new WebClient();
-
         #region IUrlDownloader Members
 
         public string Download(string url)
         {
-            return _webClient.DownloadString(url);
+            return new WebClient().DownloadString(url);
         }
 
         public void DownloadFile(string url, string filename)
         {
-            _webClient.DownloadFile(url, filename);
+            new WebClient().DownloadFile(url, filename);
         }
 
         #endregion
