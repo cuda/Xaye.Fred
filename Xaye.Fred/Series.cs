5523a90b24ba92abacfa1c7bfdea728964059af7
Marcus Cuda <marcus@cuda.org>
2012-10-18 11:46:28 +0300

    added new UseRealtimeFields property which defaults to false and forces the Series object to use the current CST time when retrieving data

--- Xaye.Fred/Series.cs
+++ Xaye.Fred/Series.cs
@@ -1,20 +1,23 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Xaye.Fred
 {
     /// <summary>
-    /// An economic data series
+    ///   An economic data series
     /// </summary>
-    /// <remarks>See http://api.stlouisfed.org/docs/fred/realtime_period.html for information 
-    /// about real-time periods.</remarks>
+    /// <remarks>
+    ///   See http://api.stlouisfed.org/docs/fred/realtime_period.html for information 
+    ///   about real-time periods.
+    /// </remarks>
     public class Series : Item, IEnumerable<Observation>
     {
         #region FilterBy enum
 
         /// <summary>
-        /// What to filter by when retrieving series. Defaults to FilterBy.None.
+        ///   What to filter by when retrieving series. Defaults to FilterBy.None.
         /// </summary>
         public enum FilterBy
         {
@@ -29,7 +32,7 @@ namespace Xaye.Fred
         #region OrderBy enum
 
         /// <summary>
-        /// When retrieving data, what to order the data by. Defaults to OrderBy.SeriesId.
+        ///   When retrieving data, what to order the data by. Defaults to OrderBy.SeriesId.
         /// </summary>
         public enum OrderBy
         {
@@ -52,21 +55,22 @@ namespace Xaye.Fred
         #region UpdateFilterBy enum
 
         /// <summary>
-        /// What to filter by when retrieving series updates. Defaults to FilterBy.All.
+        ///   What to filter by when retrieving series updates. Defaults to FilterBy.All.
         /// </summary>
         public enum UpdateFilterBy
         {
             /// <summary>
-            /// Does not filter results.
+            ///   Does not filter results.
             /// </summary>
             All,
+
             /// <summary>
-            ///  Representing the entire United States. 
+            ///   Representing the entire United States.
             /// </summary>
             Macro,
 
             /// <summary>
-            /// Limits results to series for parts of the US; namely, series for US states, counties, and Metropolitan Statistical Areas (MSA). 
+            ///   Limits results to series for parts of the US; namely, series for US states, counties, and Metropolitan Statistical Areas (MSA).
             /// </summary>
             Regional
         }
@@ -74,70 +78,77 @@ namespace Xaye.Fred
         #endregion
 
         private volatile IEnumerable<Category> _categories;
-        private volatile Release _release;
         private volatile List<Observation> _data;
+        private volatile Release _release;
 
         internal Series(Fred fred) : base(fred)
         {
         }
 
         /// <summary>
-        /// Gets the series id.
+        ///   Gets the series id.
         /// </summary>
         public string Id { get; set; }
-       
+
         /// <summary>
-        /// Gets the series title.
+        ///   Gets the series title.
         /// </summary>
         public string Title { get; set; }
 
         /// <summary>
-        /// Gets the start of the real-time period.
+        ///   Gets the start of the real-time period.
         /// </summary>
         public DateTime RealtimeStart { get; set; }
-       
+
         /// <summary>
-        /// Gets the end of the real-time period.
+        ///   Gets the end of the real-time period.
         /// </summary>
         public DateTime RealtimeEnd { get; set; }
-       
+
         /// <summary>
-        /// Gets the start of the observation period..
+        ///   Gets the start of the observation period..
         /// </summary>
         public DateTime ObservationStart { get; set; }
 
         /// <summary>
-        /// Gets the end of the observation period.
+        ///   Gets the end of the observation period.
         /// </summary>
         public DateTime ObservationEnd { get; set; }
 
         /// <summary>
-        /// Gets the series's frequency.
+        ///   Gets the series's frequency.
         /// </summary>
         public Frequency Frequency { get; set; }
-        
+
         /// <summary>
-        /// Gets the series unit of measurement.
-        /// </summary>    
+        ///   Gets the series unit of measurement.
+        /// </summary>
         public string Units { get; set; }
 
         /// <summary>
-        /// Gets a value indicating whether series is seasonally adjusted.
+        ///   Whether to use <see cref="RealtimeStart" /> and <see cref="RealtimeEnd" /> 
+        ///   when making FRED calls (such as getting observations) or not. If false,
+        ///   then the current day is used.
+        /// </summary>
+        public bool UseRealtimeFields { get; set; }
+
+        /// <summary>
+        ///   Gets a value indicating whether series is seasonally adjusted.
         /// </summary>
         public bool SeasonalAdjusted { get; set; }
-        
+
         /// <summary>
-        /// Gets the date the series was last updated.
+        ///   Gets the date the series was last updated.
         /// </summary>
         public DateTime LastUpdated { get; set; }
-        
+
         /// <summary>
-        /// Gets the series popularity.
+        ///   Gets the series popularity.
         /// </summary>
         public int Popularity { get; set; }
 
         /// <summary>
-        /// Gets the release the series belongs to. Lazily loaded.
+        ///   Gets the release the series belongs to. Lazily loaded.
         /// </summary>
         public Release Release
         {
@@ -149,7 +160,9 @@ namespace Xaye.Fred
                     {
                         if (_release == null)
                         {
-                            _release = Fred.GetSeriesRelease(Id, RealtimeStart, RealtimeEnd);
+                            _release = UseRealtimeFields
+                                           ? Fred.GetSeriesRelease(Id, RealtimeStart, RealtimeEnd)
+                                           : Fred.GetSeriesRelease(Id);
                         }
                     }
                 }
@@ -159,9 +172,10 @@ namespace Xaye.Fred
         }
 
         /// <summary>
-        /// Gets the categories the series belongs to. Lazily loaded.
+        ///   Gets the categories the series belongs to. Lazily loaded.
         /// </summary>
-        /// <remarks></remarks>
+        /// <remarks>
+        /// </remarks>
         public IEnumerable<Category> Categories
         {
             get
@@ -172,7 +186,9 @@ namespace Xaye.Fred
                     {
                         if (_categories == null)
                         {
-                            _categories = Fred.GetSeriesCategories(Id, RealtimeStart, RealtimeEnd);
+                            _categories = UseRealtimeFields
+                                              ? Fred.GetSeriesCategories(Id, RealtimeStart, RealtimeEnd)
+                                              : Fred.GetSeriesCategories(Id);
                         }
                     }
                 }
@@ -181,9 +197,10 @@ namespace Xaye.Fred
         }
 
         /// <summary>
-        /// Gets the series observations. Lazily loaded.
+        ///   Gets the series observations. Lazily loaded.
         /// </summary>
-        /// <remarks></remarks>
+        /// <remarks>
+        /// </remarks>
         public IEnumerable<Observation> Observations
         {
             get
@@ -195,15 +212,24 @@ namespace Xaye.Fred
                         if (_data == null)
                         {
                             const int limit = 100000;
-                            _data = (List<Observation>) Fred.GetSeriesObservations(Id, ObservationStart, ObservationEnd, RealtimeStart,
-                                                            RealtimeEnd, Enumerable.Empty<DateTime>(), limit, 0);
+                            _data = UseRealtimeFields
+                                        ? (List<Observation>)
+                                          Fred.GetSeriesObservations(Id, ObservationStart, ObservationEnd, RealtimeStart,
+                                                                     RealtimeEnd, Enumerable.Empty<DateTime>())
+                                        : (List<Observation>)
+                                          Fred.GetSeriesObservations(Id, ObservationStart, ObservationEnd);
+
                             var count = _data.Count;
                             var call = 1;
                             while (count == limit)
                             {
-                                var more = (List<Observation>) Fred.GetSeriesObservations(Id, ObservationStart, ObservationEnd, RealtimeStart,
-                                                                RealtimeEnd, Enumerable.Empty<DateTime>(), limit,
-                                                                call*limit);
+                                var start = UseRealtimeFields ? RealtimeStart : Fred.CstTime();
+                                var end = UseRealtimeFields ? RealtimeEnd : Fred.CstTime();
+                                var more =
+                                    (List<Observation>)
+                                    Fred.GetSeriesObservations(Id, ObservationStart, ObservationEnd, start,
+                                                               end, Enumerable.Empty<DateTime>(), limit,
+                                                               call*limit);
                                 _data.AddRange(more);
                                 count = more.Count;
                                 call++;
@@ -216,24 +242,30 @@ namespace Xaye.Fred
             }
         }
 
+        #region IEnumerable<Observation> Members
+
         /// <summary>
-        /// Returns an enumerator that iterates through the collection.
+        ///   Returns an enumerator that iterates through the collection.
         /// </summary>
-        /// <returns>A <see cref="T:System.Collections.Generic.IEnumerator`1"/> that can be used to iterate through the collection.</returns>
-        /// <remarks></remarks>
+        /// <returns> A <see cref="T:System.Collections.Generic.IEnumerator`1" /> that can be used to iterate through the collection. </returns>
+        /// <remarks>
+        /// </remarks>
         public IEnumerator<Observation> GetEnumerator()
         {
             return Observations.GetEnumerator();
         }
 
         /// <summary>
-        /// Returns an enumerator that iterates through a collection.
+        ///   Returns an enumerator that iterates through a collection.
         /// </summary>
-        /// <returns>An <see cref="T:System.Collections.IEnumerator"/> object that can be used to iterate through the collection.</returns>
-        /// <remarks></remarks>
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        /// <returns> An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection. </returns>
+        /// <remarks>
+        /// </remarks>
+        IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
         }
+
+        #endregion
     }
 }
